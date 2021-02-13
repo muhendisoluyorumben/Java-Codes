f75a13f7866d2603ebdeb1a600ce80425b17d794
muhendisoluyorumben <78914293+muhendisoluyorumben@users.noreply.github.com>
2021-02-13 13:53:07 +0300

    Add files via upload

---
+++ Calisma010_StringTersCevirme.java
@@ -0,0 +1,24 @@
+package Calismalar;
+
+/**
+ * Girilen String deðiþkeni ters çeviren program.
+ * @author Murat
+ *	07.02.2021 / 20.47
+ */
+import java.util.Scanner;
+
+public class Calisma010_StringTersCevirme {
+	public static void main(String[] args) {
+		String mesaj;
+		char hadibakým;
+		System.out.println("yazý giriniz");
+		Scanner klavye= new Scanner(System.in);
+		mesaj=klavye.nextLine();
+		for(int i=mesaj.length()-1;i>=0;i--) {
+		hadibakým=mesaj.charAt(i);  // Stringi char' a çevirme.
+		System.out.print(hadibakým);
+		}
+		klavye.close();
+	}
+
+}
