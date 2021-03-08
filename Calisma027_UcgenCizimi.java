c3be23b14df333d729beba197fe0797d44a5b57e
Murat Sahin <78914293+muhendisoluyorumben@users.noreply.github.com>
2021-03-08 21:42:55 +0300

    Add files via upload

---
+++ Calisma027_UcgenCizimi.java
@@ -0,0 +1,31 @@
+package Calismalar;
+/**
+ * '*' karakteri ile ucgen cizen program. 
+ * @author Murat
+ * 
+ * 03.03.2021 / 17.53
+ **/
+import java.util.Scanner;
+
+public class Calisma027_UcgenCizimi {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner klavye=new Scanner(System.in);
+		System.out.println("Ucgeninin yuksekligini giriniz.");
+		n=klavye.nextInt();
+		
+		for(int i=0;i<=n;i++) {
+			for(int j=n-i;j>=0;j--) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<(2*i-1);k++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		klavye.close();
+
+	}
+
+}
